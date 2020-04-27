43bb13e4219bf3d3f2589840c9c0d1bc54755277
Jahziel Villasana <jahzielv@gmail.com>
2020-04-27 12:24:31 -0400

    more promise wrangling

--- functions/fb-auth.js
+++ functions/fb-auth.js
@@ -7,15 +7,12 @@ exports.handler = async (event, context) => {
     const data = event["queryStringParameters"]["code"];
     console.log("Function `fb-auth` invoked", data);
 
-    const promise = new Promise((resolve, reject) => {
-        fetch(
-            `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
-                REDIRECT_URI
-            )}&client_secret=${FB_APP_SECRET}&code=${data}`
-        )
-            .then((res) => res.json())
-            .then((data) => resolve(data))
-            .catch((err) => reject(err));
-    });
-    return promise;
+    return fetch(
+        `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
+            REDIRECT_URI
+        )}&client_secret=${FB_APP_SECRET}&code=${data}`
+    )
+        .then((res) => res.json())
+        .then((jsonData) => jsonData)
+        .catch((err) => err);
 };
