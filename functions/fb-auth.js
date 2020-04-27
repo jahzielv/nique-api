b76c4601c7516837871a874167dfdb1ea0a62488
Jahziel Villasana <jahzielv@gmail.com>
2020-04-27 12:58:21 -0400

    getting somewhere...

--- functions/fb-auth.js
+++ functions/fb-auth.js
@@ -8,14 +8,19 @@ exports.handler = async (event, context) => {
     const data = event["queryStringParameters"]["code"];
     console.log("Function `fb-auth` invoked", data);
 
-    let response = await fetch(
-        `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
-            REDIRECT_URI
-        )}&client_secret=${FB_APP_SECRET}&code=${data}`
-    );
-    console.log("fetched data");
-    let jsonData = await response.json();
-    console.log(`got json data: ${jsonData}`);
-    const apiRes = { statusCode: 202, body: jsonData };
-    return apiRes;
+    try {
+        let response = await fetch(
+            `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
+                REDIRECT_URI
+            )}&client_secret=${FB_APP_SECRET}&code=${data}`
+        );
+        console.log("fetched data");
+        let jsonData = await response.json();
+        console.log(`got json data: ${util.inspect(jsonData)}`);
+        const apiRes = { statusCode: 202, body: jsonData };
+        return apiRes;
+    } catch (error) {
+        console.log("ERROR", error);
+        return { statusCode: 511, body: "we big failed" };
+    }
 };
