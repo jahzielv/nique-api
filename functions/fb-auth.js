f83f426e52d4769b6868fa5e0a33fafc3cad60c0
Jahziel Villasana <jahzielv@gmail.com>
2020-04-27 11:07:46 -0400

    added uri encoding for redirect uri

--- functions/fb-auth.js
+++ functions/fb-auth.js
@@ -4,7 +4,9 @@ const FB_APP_SECRET = process.env.FB_APP_SECRET;
 
 async function getData() {
     let response = await fetch(
-        `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${REDIRECT_URI}&client_secret=${FB_APP_SECRET}&code=${data}`
+        `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
+            REDIRECT_URI
+        )}&client_secret=${FB_APP_SECRET}&code=${data}`
     );
     let json = await response.json();
     return json;
