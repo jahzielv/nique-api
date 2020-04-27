040ca36c11dfb7fc46b8db81393dc9b171596bd3
Jahziel Villasana <jahzielv@gmail.com>
2020-04-27 12:54:30 -0400

    more function debugging

--- functions/fb-auth.js
+++ functions/fb-auth.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const util = require("util");
 const FB_APP_ID = process.env.FB_APP_ID;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
@@ -7,21 +8,14 @@ exports.handler = async (event, context) => {
     const data = event["queryStringParameters"]["code"];
     console.log("Function `fb-auth` invoked", data);
 
-    return fetch(
+    let response = await fetch(
         `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${FB_APP_ID}&redirect_uri=${encodeURIComponent(
             REDIRECT_URI
         )}&client_secret=${FB_APP_SECRET}&code=${data}`
-    )
-        .then((res) => {
-            console.log("resolving response");
-            return res.json();
-        })
-        .then((jsonData) => {
-            console.log("resolving json data", jsonData);
-            return jsonData;
-        })
-        .catch((err) => {
-            console.log("error: ", err);
-            return err;
-        });
+    );
+    console.log("fetched data");
+    let jsonData = await response.json();
+    console.log(`got json data: ${jsonData}`);
+    const apiRes = { statusCode: 202, body: jsonData };
+    return apiRes;
 };
